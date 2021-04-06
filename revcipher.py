55d5e8eec9cba58766f1c880ca4f90cc4fc34a3d
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-04-06 11:28:54 -0500

    RevCipher

--- revcipher.py
+++ revcipher.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-
+#importamos modulos basicos.
 from colorama import Fore, init
 init()
 import time
 import os
 
 def switch():
+	#funcion definida del menu de opciones.
 	os.system('clear')
 	print(Fore.GREEN)
 	print("Hola, bienvenido a mi programa...")
@@ -14,12 +15,16 @@ def switch():
 	print("Escoge una opcion: \n1- Encriptar Mensaje. \n2-Desencriptar Mensaje.")
 
 while True:
+	#Cuando sea verdadera la condicion, se cumplira un bucle del menu.
 	switch()
 	ask = int(input("\n>>> "))
+	#primer condicional de nuestro programa.
 	if ask==1:
+		#input: se le preguntara el mensaje al usuario.
 		message = str(input("Escribe aqui tu mensaje para Encriptar: "))
 		translated = ""
 		i = len(message)-1
+		#Primer ciclo while, esto hara el proceso de encriptacion reversa en nuestro mensaje.
 		while i>=0:
 			translated= translated + message[i]
 			i= i-1
@@ -28,9 +33,11 @@ while True:
 		input()
 
 	elif ask==2:
+		#Segundo condicional de nuestro programa.
 		message = str(input("Escribe aqui tu mensaje a Desencriptar: "))
 		translated = ""
 		i = len(message)-1
+		#ciclo while para nuestra condicion, esto hara el proceso de desencriptacion reversa del mensaje.
 		while i>=0:
 			translated = translated+message[i]
 			i = i - 1
